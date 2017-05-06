f78d214b8145e5047e947e449b906e5f6ddfbd16
Mehdi Bizhani <mbizhani@gmail.com>
2017-05-06 09:48:44 +0430

    defining filters

--- src/main/java/org/devocative/devolcano/maven/ApplySchemaMavenPlugin.java
+++ src/main/java/org/devocative/devolcano/maven/ApplySchemaMavenPlugin.java
@@ -2,19 +2,22 @@ package org.devocative.devolcano.maven;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Execute;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.plugins.annotations.*;
 import org.devocative.demeter.core.DemeterCore;
 
 @Mojo(name = "applyschema", requiresDependencyResolution = ResolutionScope.TEST)
 @Execute(phase = LifecyclePhase.TEST_COMPILE)
 public class ApplySchemaMavenPlugin extends AbstractDemeterMavenPlugin {
 
+	@Parameter
+	private String filters;
+
 	@Override
 	public void doExecute() throws MojoExecutionException, MojoFailureException {
-		DemeterCore.applySQLSchemas();
+		String[] filterArray = null;
+		if (filters != null) {
+			filterArray = filters.split("[,]");
+		}
+		DemeterCore.applySQLSchemas(filterArray);
 	}
-
 }
